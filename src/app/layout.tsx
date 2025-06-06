bf3450bcc3815ef9f5286b22d55cb319688a61dc
Randy Tolentino <randytolentino@ibm.com>
2025-06-06 07:02:12 -0500

    feat: update meta info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
+import { AUTHOR_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import cn from "classnames";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "600"],
+});
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `${AUTHOR_NAME} | Portfolio`,
+  description: `work and thoughts by ${AUTHOR_NAME}.`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
